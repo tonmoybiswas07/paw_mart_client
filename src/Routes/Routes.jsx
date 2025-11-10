529c60011ec5e12243e680268544902607a4c7bc
tonmoybiswas9 <tonmoy.b3399@gmail.com>
2025-11-10 11:32:21 +0600

    added initial setup

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router";
+import Root from "../Components/Root/Root";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+  },
+]);
+export default router;
