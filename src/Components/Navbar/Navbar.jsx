a48b586f0087ab63b2a3624293a6879fec926488
tonmoybiswas9 <tonmoy.b3399@gmail.com>
2025-11-10 12:20:06 +0600

    added navbar

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { Link } from "react-router";
+
+const Navbar = () => {
+  const links = (
+    <>
+      <Link>Home</Link>
+      <Link>Pets &amp; Supplies</Link>
+      <Link>Add Listing</Link>
+      <Link>My Listings</Link>
+      <Link>My Orders</Link>
+    </>
+  );
+  return (
+    <div>
+      <div className="navbar bg-base-100 fixed top-0 left-0 right-0 shadow">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                {" "}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />{" "}
+              </svg>
+            </div>
+            <ul
+              tabIndex="-1"
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              {links}
+            </ul>
+          </div>
+          <Link className=" text-xl font-bold text-amber-600">PawMart</Link>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1 gap-5">{links}</ul>
+        </div>
+        <div className="navbar-end gap-3">
+          <Link className="btn bg-amber-500 text-white">Login</Link>
+          <Link className="btn bg-amber-500 text-white">Register</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/Components/Root/Root.jsx
+++ src/Components/Root/Root.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import Navbar from "../Navbar/Navbar";
+import { Outlet } from "react-router";
 
 const Root = () => {
   return (
     <div>
-      <h2>hello world</h2>
+      <Navbar></Navbar>
+      <Outlet></Outlet>
     </div>
   );
 };
